69874fc8a39129b9b7e8a29dc12ba036249a35ce
miguelC1 <miguel68933886@gmail.com>
2023-11-21 06:41:08 -0400

    creciond el proteyecion gestion de rol

---
+++ src/main/java/com/eeds/monolitico/GestionRoles/GestionRolesApplication.java
@@ -0,0 +1,13 @@
+package com.eeds.monolitico.GestionRoles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionRolesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionRolesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eeds/monolitico/GestionRoles/GestionRolesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eeds.monolitico.GestionRoles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionRolesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
